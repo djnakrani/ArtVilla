3982b8769e920ab6009f69919f35a1500a9d136e
Dharmik Nakrani <dharmiknakrani1690@gmail.com>
2021-07-18 18:10:20 +0530

    Splash Activity Done

--- app/src/main/java/com/example/artvilla/login.java
+++ app/src/main/java/com/example/artvilla/login.java
@@ -6,12 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
+public class login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
 
     }
